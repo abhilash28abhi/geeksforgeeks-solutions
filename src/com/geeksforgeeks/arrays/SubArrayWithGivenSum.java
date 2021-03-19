eda9433a06cc5f887c9cd4f5ca726aba103f9230
abhilash28abhi <abhilash28.abhi@gmail.com>
2021-03-19 19:45:27 +0530

    Initial commit

---
+++ src/com/geeksforgeeks/arrays/SubArrayWithGivenSum.java
@@ -0,0 +1,15 @@
+package com.geeksforgeeks.arrays;
+
+/**
+ * Given an unsorted array of nonnegative integers, find a continuous subarray which adds to a given number.
+ * Input: arr[] = {1, 4, 20, 3, 10, 5}, sum = 33
+ * Ouptut: Sum found between indexes 2 and 4
+ * Sum of elements between indices
+ * 2 and 4 is 20 + 3 + 10 = 33
+ */
+public class SubArrayWithGivenSum {
+
+    public static void main(String[] args) {
+
+    }
+}
