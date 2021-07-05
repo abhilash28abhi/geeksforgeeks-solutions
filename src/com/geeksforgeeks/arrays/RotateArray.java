ad8b7bf69eb3cacae3a2c68ea71771f7750f83b4
abhilash28abhi <abhilash28.abhi@gmail.com>
2021-07-05 20:07:11 +0530

    Changed the logic

--- src/com/geeksforgeeks/arrays/RotateArray.java
+++ src/com/geeksforgeeks/arrays/RotateArray.java
@@ -5,28 +5,27 @@ import java.util.Arrays;
 /**
  * Write a function rotate(ar[], d, n) that rotates arr[] of size n by d elements.
  * input: 1 2 3 4 5 6 7 ; d = 2
- * output: 7 6 1 2 3 4 5
+ * output: 6 7 1 2 3 4 5
  */
 public class RotateArray {
 
     private static void rotateArray(int[] arr, int count) {
         int[] output = new int[arr.length];
+        int j = 0;
 
-        int p = count;
-        for (int i = 0; i < arr.length - count; i++) {
-            output[p] = arr[i];
-            p++;
+        for (int i = 0 ; i < arr.length; i++) {
+            if (i < count) {
+                output[j] = arr[arr.length - (count - i)];
+            } else {
+                output[j] = arr[i - count];
+            }
+            j++;
         }
 
-        int k = 0;
-        int i = arr.length - 1;
-        while (k < count) {
-            output[k++] = arr[i--];
-        }
         System.out.println("Rotated array is : " + Arrays.toString(output));
     }
 
     public static void main(String[] args) {
-        rotateArray(new int[]{1, 2, 3, 4, 5, 6, 7}, 4);
+        rotateArray(new int[]{1, 2, 3, 4, 5, 6, 7}, 3);
     }
 }
