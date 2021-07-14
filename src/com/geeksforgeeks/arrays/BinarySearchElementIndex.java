769e6dda040a378883c0680186b82443f1fa1ab3
abhilash28abhi <abhilash28.abhi@gmail.com>
2021-07-14 19:56:02 +0530

    Binary search element index

---
+++ src/com/geeksforgeeks/arrays/BinarySearchElementIndex.java
@@ -0,0 +1,27 @@
+package com.geeksforgeeks.arrays;
+
+public class BinarySearchElementIndex {
+
+    public static void main(String[] args) {
+        System.out.println("Element found at index: " +
+                binarySearchIndex(new int[]{1, 2, 4, 5, 6}, 3)); //output should be 4
+    }
+
+    private static int binarySearchIndex(int[] arr, int element) {
+        int first = 0;
+        int last = arr.length;
+        int mid = first + (last - first)/2;
+
+        while (first <= last) {
+            if (element < arr[mid]) {
+                last = mid - 1;
+            } else if (element == arr[mid]) {
+                return mid;
+            } else {
+                first = mid + 1;
+            }
+            mid = first + (last - first)/2;
+        }
+        return last + 1;
+    }
+}
