16bdc0f91e2584601d0e79f2feb6fa24337051f1
abhilash28abhi <abhilash28.abhi@gmail.com>
2021-08-26 20:03:40 +0530

    Basic maths programs

---
+++ src/com/geeksforgeeks/mathematics/Factorial.java
@@ -0,0 +1,15 @@
+package com.geeksforgeeks.mathematics;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+        System.out.println(factorialOfNumber(5));
+    }
+
+    private static int factorialOfNumber(int n) {
+        if (n == 1 || n == 0) {
+            return 1;
+        }
+        return n * factorialOfNumber(n - 1);
+    }
+}
