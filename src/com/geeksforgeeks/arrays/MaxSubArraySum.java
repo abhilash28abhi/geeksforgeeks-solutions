ea28824a2e7ccaf184a568c4dd28b4a4f5de5faa
abhilash28abhi <abhilash28.abhi@gmail.com>
2021-09-15 20:42:02 +0530

    Kadane's algo

---
+++ src/com/geeksforgeeks/arrays/MaxSubArraySum.java
@@ -0,0 +1,44 @@
+package com.geeksforgeeks.arrays;
+
+public class MaxSubArraySum {
+
+    public static void main(String[] args) {
+        System.out.println("Maximum contiguous sum is : " + findMaxSubArraySum(new int[]{-5, 4, 6, -3, 4, -1}));
+        System.out.println("Maximum contiguous sum using Kadane's Algo is : " + kadaneAlgo(new int[]{-5, 4, 6, -3, 4, -1}));
+    }
+
+    // TC O(n^2)
+    private static int findMaxSubArraySum(int[] input) {
+        int maxSum = Integer.MIN_VALUE;
+        int sum = 0;
+
+        for(int i = 0; i < input.length; i++) {
+            sum = 0;
+            for(int j = i; j < input.length; j++) {
+                sum = sum + input[j];
+                if (sum > maxSum) {
+                    maxSum = sum;
+                }
+            }
+        }
+        return maxSum;
+    }
+
+    // TC O(n)
+    private static int kadaneAlgo(int[] input) {
+        int maxSum = Integer.MIN_VALUE;
+        int sum = 0;
+
+        for(int i = 0; i < input.length; i++) {
+            sum = sum + input[i];
+
+            if (sum > maxSum) {
+                maxSum = sum;
+            }
+            if(sum < 0) {
+                sum = 0;
+            }
+        }
+        return maxSum;
+    }
+}
