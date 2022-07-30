8370b582383f69b0f9f646877b9449b5f9d83423
abhilash28abhi <abhilash28.abhi@gmail.com>
2022-07-30 14:14:22 +0530

    LCS tabulation method

--- src/com/geeksforgeeks/dynamicprog/LongestCommonSubsequence.java
+++ src/com/geeksforgeeks/dynamicprog/LongestCommonSubsequence.java
@@ -20,6 +20,8 @@ public class LongestCommonSubsequence {
             Arrays.fill(row, -1);
         }
         System.out.println("LCS with DP is : " + findLcsWithDp(str1.length(), str2.length(), str1, str2, dp));
+        int[][] tabulationMat = new int[str1.length() + 1][str2.length() + 1];
+        System.out.println("LCS with tabulation method is : " + findLcsWithTabulation(str1.length(), str2.length(), str1, str2, tabulationMat));
     }
 
     //TC here is O(2^N)
@@ -51,5 +53,26 @@ public class LongestCommonSubsequence {
         } else {
             return dp[m][n] = Math.max(findLcsWithDp(m-1, n, str1, str2, dp), findLcsWithDp(m, n-1, str1, str2, dp));
         }
+    }
+
+    //Here each position in the array will be evaluated and filled
+    //TC is O(N*M)
+    private static int findLcsWithTabulation(int m, int n, String str1, String str2, int[][] tabulationMat) {
+        if (m == 0 || n == 0) {
+            return 0;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    //if the last char are same in both strings we have 1 common subsequence
+                    tabulationMat[i][j] = 1 + tabulationMat[i - 1][j - 1];
+                } else {
+                    //find common subsequence by dropping last char from both string one by one
+                    tabulationMat[i][j] = Math.max(tabulationMat[i-1][j], tabulationMat[i][j -1]);
+                }
+            }
+        }
+        return tabulationMat[m][n];
     }
 }
